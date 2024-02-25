ae0622050833d6fa45a22017b4136406f5aadef4
grasieletinoco <grasieleprt@gmail.com>
2024-02-24 22:23:41 -0300

    Site De Adoção De Animais Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'site-adocao-de-animais-angular';
+  title = 'Site De Adoção De Animais - Angular';
 }
