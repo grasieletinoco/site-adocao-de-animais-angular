023976608c0ce175b7f4ec30fb8f0441aa134d54
grasieletinoco <grasieleprt@gmail.com>
2024-02-28 12:21:32 -0300

    ADD Card Cat

---
+++ src/app/components/card-cat/card-cat.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CardCatComponent } from './card-cat.component';
+
+describe('CardCatComponent', () => {
+  let component: CardCatComponent;
+  let fixture: ComponentFixture<CardCatComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CardCatComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(CardCatComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/card-cat/card-cat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-cat',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-cat.component.html',
+  styleUrl: './card-cat.component.css'
+})
+export class CardCatComponent {
+
+}
