715f88b8beec1c7a8b3b46e910dac33bdced9480
grasieletinoco <grasieleprt@gmail.com>
2024-02-28 16:07:03 -0300

    ADD Conteúdo Page Dog

--- src/app/components/card-dog/card-dog.component.ts
+++ src/app/components/card-dog/card-dog.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IPet } from '../../model/ipet';
+import { DogComponent } from '../../pages/dog/dog.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-dog',
   standalone: true,
-  imports: [],
+  imports: [DogComponent, CommonModule],
   templateUrl: './card-dog.component.html',
   styleUrl: './card-dog.component.css'
 })
 export class CardDogComponent {
-
+  @Input() cardCachorro?:IPet[] = [];
+  iconDog = "&#128021;";
 }
