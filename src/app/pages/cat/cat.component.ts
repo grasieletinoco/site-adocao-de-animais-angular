df5c04e66805984fc9feb01fde0b7b0ee20d4d40
grasieletinoco <grasieleprt@gmail.com>
2024-02-28 12:23:14 -0300

    ADD Page Cat

---
+++ src/app/pages/cat/cat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cat',
+  standalone: true,
+  imports: [],
+  templateUrl: './cat.component.html',
+  styleUrl: './cat.component.css'
+})
+export class CatComponent {
+
+}
