aa08e6faed653e72de7fae7b26535850210927c0
grasieletinoco <grasieleprt@gmail.com>
2024-02-28 12:06:42 -0300

    Add Interface

---
+++ src/app/model/ipet.ts
@@ -0,0 +1,11 @@
+export interface IPet {
+    id:number;
+    img:string;
+    nome:string;
+    genero:string;
+    idade:number;
+    castrado:boolean;
+    vermifugado:boolean;
+    porte:string;
+    termos:string;
+}
