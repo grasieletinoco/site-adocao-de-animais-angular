ab7491e5fc92a343aa466688d9b597b77348f7b6
grasieletinoco <grasieleprt@gmail.com>
2024-02-28 17:53:57 -0300

    ADD Conteúdo Page Cat

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { DogComponent } from './pages/dog/dog.component';
+import { CatComponent } from './pages/cat/cat.component';
 
 export const routes: Routes = [
     {path:'',title:'Home',component:HomeComponent},
     {path:'sobre',title:'Sobre',component:AboutComponent},
     {path:'contato',title:'Contato',component:ContactComponent},
     {path:'cachorro', title:'Cachorro', component:DogComponent},
+    {path:'gato', title:'Gato',component:CatComponent},
 ];
--- src/app/components/card-cat/card-cat.component.ts
+++ src/app/components/card-cat/card-cat.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IPet } from '../../model/ipet';
+import { CatComponent } from '../../pages/cat/cat.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-cat',
   standalone: true,
-  imports: [],
+  imports: [CatComponent,CommonModule],
   templateUrl: './card-cat.component.html',
   styleUrl: './card-cat.component.css'
 })
 export class CardCatComponent {
-
+  @Input() cardGato?:IPet[] = [];
+  iconCat = "&#x1F408;";
 }
