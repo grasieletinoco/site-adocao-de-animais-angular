8a3fba001a0c554f287ba0564be49df27e06783e
grasieletinoco <grasieleprt@gmail.com>
2024-03-05 20:20:29 -0300

    ADD Rotas

--- src/app/components/card-adocao/card-adocao.component.ts
+++ src/app/components/card-adocao/card-adocao.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from '../../pages/home/home.component';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-card-adocao',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, CommonModule,RouterLink],
   templateUrl: './card-adocao.component.html',
   styleUrl: './card-adocao.component.css'
 })
 export class CardAdocaoComponent {
-
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule,RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
