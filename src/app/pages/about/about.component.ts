c0a5523d1088f7ad8619ab7313d354abf42e86ea
grasieletinoco <grasieleprt@gmail.com>
2024-03-06 01:15:37 -0300

    ADD Conteúdo Página Sobre

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,NavbarComponent,FooterComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
