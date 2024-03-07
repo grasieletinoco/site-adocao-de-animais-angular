4357bd2135109ed6223be551446534435d612cdc
grasieletinoco <grasieleprt@gmail.com>
2024-03-07 00:58:09 -0300

    ADD Conteúdo Página Contato

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NavbarComponent,FooterComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+  iconPaw="&#x1F43E";
 }
